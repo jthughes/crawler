0d703ee249898617130f77ad61a8ea122b9c578e
Jeremy Hughes <jthughes@users.noreply.github.com>
2025-10-06 08:41:03 +1030

    Initial cmdliine args

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	fmt.Println("starting crawl of:", args[0])
 }
